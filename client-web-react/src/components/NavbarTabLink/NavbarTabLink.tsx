d3dc93456ff77098b328ba95e223d17fea4df433
Alex <alexbiolete@pm.me>
2021-10-01 23:14:19 +0300

    Changed NavbarTabLink styling to match Mobile app style

--- client-web-react/src/components/NavbarTabLink/NavbarTabLink.tsx
+++ client-web-react/src/components/NavbarTabLink/NavbarTabLink.tsx
@@ -8,9 +8,9 @@ const NavbarTabLink: FC<any> = ({ label, icon, path }) => {
     <Link to={path} className="flex-1">
       <p className={`flex flex-col items-center space-y-1
         ${"/".concat(location.pathname.split('/')[1]) !== path ?
-          'hover:bg-green-50 text-green-800 hover:text-green-700 transition ease-in-out duration-300'
+          'text-gray-500 hover:text-green-500 transition ease-in-out duration-300'
         :
-          'bg-green-50 text-green-700'
+          'text-green-700'
         }
         px-2 py-2 rounded-xl
       `}>
