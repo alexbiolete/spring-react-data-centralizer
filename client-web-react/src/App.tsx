d621afaad23a9cc16be80f625ebcb8eba1d6161e
Alex <alexbiolete@pm.me>
2021-10-01 18:42:13 +0300

    Implemented methods to access the microservices from the server (data fetching, file uploading, calling methods) and replaced placeholder data with fetched data

--- client-web-react/src/App.tsx
+++ client-web-react/src/App.tsx
@@ -1,22 +1,134 @@
-import { FC, useState } from 'react';
+import {
+  FC,
+  useState,
+  useEffect,
+  ChangeEvent,
+  FormEvent
+} from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import NotFound from './views/NotFound/NotFound';
 import Dashboard from './views/Dashboard/Dashboard';
 import ImportForm from './views/ImportForm/ImportForm';
-import { First } from './types/First';
 import GeneratedTable from './views/GeneratedTable/GeneratedTable';
 import FirstTable from './views/FirstTable/FirstTable';
 import SecondTable from './views/SecondTable/SecondTable';
-import { generatedTablePlaceholder } from './testing/generatedTablePlaceholder';
-import { firstTablePlaceholder } from './testing/firstTablePlaceholder';
-import { secondTablePlaceholder } from './testing/secondTablePlaceholder';
+// import { generatedTablePlaceholder } from './testing/generatedTablePlaceholder';
+// import { firstTablePlaceholder } from './testing/firstTablePlaceholder';
+// import { secondTablePlaceholder } from './testing/secondTablePlaceholder';
 
 const App: FC = () => {
-  const [generatedTableData, setGeneratedTableData] = useState(generatedTablePlaceholder);
-  const [firstTableData, setFirstTableData] = useState(firstTablePlaceholder);
-  const [secondTableData, setSecondTableData] = useState(secondTablePlaceholder);
+  const [generatedTableData, setGeneratedTableData] = useState([]);
+  const [firstTableData, setFirstTableData] = useState([]);
+  const [secondTableData, setSecondTableData] = useState([]);
+
+  useEffect(() => {
+    const getGenerated = async() => {
+      const generatedFromServer = await fetchGenerated();
+      setGeneratedTableData(generatedFromServer);
+    };
+
+    getGenerated();
+  }, []);
+
+  useEffect(() => {
+    const getFirst = async() => {
+      const firstFromServer = await fetchFirst();
+      setFirstTableData(firstFromServer);
+    };
+
+    getFirst();
+  }, []);
+
+  useEffect(() => {
+    const getSecond = async() => {
+      const secondFromServer = await fetchSecond();
+      setSecondTableData(secondFromServer);
+    };
+
+    getSecond();
+  }, []);
+
+  const fetchGenerated = async () => {
+    const res = await fetch(
+      `http://localhost:8080/api/generated`
+    );
+    const data = await res.json();
+
+    console.log(data);
+
+    return data;
+  };
+
+  const generateReport = async () => {
+    const res = await fetch(
+      `http://localhost:8080/api/generated/generate`
+    );
+    const data = await res.json();
+
+    console.log(data);
+
+    return data;
+  };
+
+  const fetchFirst = async () => {
+    const res = await fetch(
+      `http://localhost:8080/api/first`
+    );
+    const data = await res.json();
+
+    console.log(data);
+
+    return data;
+  };
+
+  const importFirst = async (e: ChangeEvent | FormEvent, file: any) => {
+    const formData = new FormData();
+    formData.append('file', file, file.name);
+
+    e.preventDefault();
+
+    const res = await fetch(
+      `http://localhost:8080/api/first/import`,
+      {
+        method: 'POST',
+        body: formData
+      }
+    );
+    const data = await res.json();
+
+    console.log(data);
+  };
+
+  const fetchSecond = async () => {
+    const res = await fetch(
+      `http://localhost:8080/api/second`
+    );
+    const data = await res.json();
+
+    console.log(data);
+
+    return data;
+  };
+
+  const importSecond = async (e: ChangeEvent | FormEvent, file: any) => {
+    const formData = new FormData();
+    formData.append('file', file, file.name);
+
+    e.preventDefault();
+
+    const res = await fetch(
+      `http://localhost:8080/api/second/import`,
+      {
+        method: 'POST',
+        body: formData
+      }
+    );
+    const data = await res.json();
+
+    console.log(data);
+  };
 
   return (
     <BrowserRouter>
