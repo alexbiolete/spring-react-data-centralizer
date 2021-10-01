8e69d50780b02898841c0bd69fd6462c57216137
Alex <alexbiolete@pm.me>
2021-10-01 13:11:00 +0300

    Created Spring Boot project for the server with Spring Web, Spring Security, Spring Data JPA and MySQL Driver

---
+++ server-spring/src/test/java/com/alexbiolete/datacentralizer/DatacentralizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexbiolete.datacentralizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatacentralizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
